cda340b2f504e063678cd08836017e59fd0f1ede
Tysuiku <Timothydong41@gmail.com>
2022-10-27 23:27:07 -0400

    wentThroughBasicSyntax

--- rubyNotes.rb
+++ rubyNotes.rb
@@ -22,5 +22,22 @@
 #isMale = true
 #isFemale = false
 #flaws = nil #nil no value
-puts "ok\" guys"
-puts "ok\n guys" # \n print on new line
+#puts "ok\" guys"
+#puts "ok\n guys" # \n print on new line
+#string functions string.upcase() string.downcase() string.strip() to remove white spaces string.length() string.include?'string'
+#phrase = 'giraffe academy';
+#puts phrase[0]
+#puts phrase[0,3] => gir doesnt include a
+#phrase.index('g') 0
+
+#puts 5 + 9;
+#puts 2**3; # = 2^3
+#puts 10 % 3;
+#num = 20;
+#puts ('my favorite num is ' + num.to_s) # num.to_s convert to string
+#puts #num.abs() num.round() num.ceil() takes the higher number num.floor() takes the lower number
+#puts Math.sqrt(36)
+#puts Math.log(1)
+#puts 10 / 7 # inter divide inter only gets inter back 10 / 7.0 with decimal get decimals back (floating point number)
+
+
