864283866e4fce7e8afb1466b5e5ec61c4011f75
Tysuiku <Timothydong41@gmail.com>
2022-10-27 22:11:21 -0400

    startedRuby

---
+++ rubySyntaxNotes.rb
@@ -0,0 +1,28 @@
+#puts 'mike'
+#puts 'is cool'
+#puts prints on new lines 
+#print prints on 1 line 
+#print 'giraffe'
+#print ' is cool'
+#puts '   /|'
+#puts '  / |'
+#puts ' /  |'
+#puts '/___|'
+
+#name = 'John'
+#age = '35'
+#puts name + ' ' + age;
+#name = 'Bob'
+#puts name + ' ' + age;
+
+#varibles store string number boolean 
+#name = 'mike' 
+#age = 75
+#gpa = 3.2
+#isMale = true
+#isFemale = false
+#flaws = nil #nil no value
+puts "ok\" guys"
+puts "ok\n guys" # \n print on new line
+
+
