72a475c3c352623e81a3187225f7298be5639f19
Tysuiku <Timothydong41@gmail.com>
2022-11-19 20:52:22 -0500

    finished_bootcamp_project

--- practice_2/bootcamp_project/lib/bootcamp.rb
+++ practice_2/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,51 @@
 class Bootcamp
-  
+
+    attr_reader :name, :slogan, :teachers, :students
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan 
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new { |h, k| h[k] = []}
+    end
+
+    def hire(teacher)
+        @teachers << teacher
+    end
+
+    def enroll(student)
+        if @students.length < @student_capacity
+        @students << student 
+        return true
+        end
+        false
+    end
+
+    def enrolled?(student)
+        @students.include?(student)
+    end
+
+    def student_to_teacher_ratio
+        @students.length / @teachers.length
+    end
+
+    def add_grade(student, grade)
+        if self.enrolled?(student)
+            @grades[student] << grade
+                return true
+        end
+        false
+    end
+
+    def num_grades(student)
+        @grades[student].length
+    end
+
+    def average_grade(student)
+        if @grades[student].length < 1 || !self.enrolled?(student)
+            return nil
+        end
+        @grades[student].sum / @grades[student].length
+    end
 end
