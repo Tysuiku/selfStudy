45dd1a9e30311d3fb2a202ec027012fd556648aa
Tysuiku <Timothydong41@gmail.com>
2022-11-19 22:06:59 -0500

    finished_monkeypatch

--- practice_2/monkey_patching_project/lib/array.rb
+++ practice_2/monkey_patching_project/lib/array.rb
@@ -1,4 +1,78 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+    def span
+        if self.length < 1 
+            return nil
+        end
+        self.max - self.min 
+    end
+
+    def average
+        if self.length < 1 
+            return nil
+        end
+        self.sum / (self.length * 1.0)
+    end
+    
+    def median
+        if self.length < 1 
+            return nil
+        end
+        if self.length.odd?
+            return self.sort[self.length / 2]
+        else
+            return (self.sort[self.length / 2] + self.sort[(self.length / 2) - 1]) / 2.0
+        end
+    end
+
+    def counts 
+        hash = Hash.new(0)
+        self.each do |ele|
+           if hash[ele] == 0
+                hash[ele] += 1
+           else
+                hash[ele] += 1
+           end
+        end
+        hash
+    end
+
+    def my_count(value)
+        hash = self.counts
+            hash[value]
+    end
+
+    def my_index(value)
+        self.each_with_index do |ele, idx|
+            if ele == value 
+                return idx
+            end
+        end
+        return nil
+    end
+
+    def my_uniq 
+        uniq = []
+        self.each do |ele|
+            if !uniq.include?(ele)
+                uniq << ele
+            end
+        end
+        uniq
+    end
+
+    def my_transpose
+        new_arr = []
+        
+        (0...self.length).each do |row|
+            new_row = []
+
+            (0...self.length).each do |col|
+                new_row << self[col][row]
+            end 
+
+            new_arr << new_row
+        end
+            new_arr
+    end
 end
