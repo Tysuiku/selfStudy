218682d0340b09cb1c6a956ee23b1be048fcc232
Tysuiku <Timothydong41@gmail.com>
2022-11-19 20:24:03 -0500

    finished_dog_project

--- practice_2/dog_project/lib/dog.rb
+++ practice_2/dog_project/lib/dog.rb
@@ -1,3 +1,29 @@
 class Dog
 
+    attr_accessor :name, :breed, :age, :bark, :favorite_foods
+
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def age=(number)
+        @age = number
+    end
+
+    def bark
+        if @age > 3 
+            return @bark.upcase 
+        else 
+            return @bark.downcase
+        end
+    end
+
+    def favorite_food?(food)
+        @favorite_foods.any? { |favorite_food| favorite_food.downcase == food.downcase  }
+    end
+
 end
