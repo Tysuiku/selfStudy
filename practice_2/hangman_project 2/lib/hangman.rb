1f138f3d6f321de7c6c9e54e3ec61dad7e240f64
Tysuiku <Timothydong41@gmail.com>
2022-11-20 11:35:56 -0500

    woke_up_finished_hangman

--- practice_2/hangman_project 2/lib/hangman.rb
+++ practice_2/hangman_project 2/lib/hangman.rb
@@ -34,5 +34,53 @@ class Hangman
     end
   end
 
+  def try_guess(char)
+    if self.already_attempted?(char)
+      puts "that has already been attempted"
+        return false
+    end
+
+    @attempted_chars << char
+
+    matches = self.get_matching_indices(char)
+    self.fill_indices(char, matches)
+
+    @remaining_incorrect_guesses -= 1 if matches.empty? 
+
+    true
+  end
   
+  def ask_user_for_guess
+    puts "Enter a char"
+    guess = gets.chomp()
+    self.try_guess(guess)
+  end
+
+  def win?
+    if @guess_word.join("") == @secret_word
+      puts "WIN"
+        return true
+    end
+
+    false
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0 
+      puts "LOSE"
+        return true
+    end
+
+  false
+  end
+
+  def game_over?
+    if self.win? || self.lose?
+      puts @secret_word
+      return true
+    end
+
+    false
+  end
+
 end
