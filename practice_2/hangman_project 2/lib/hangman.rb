4a6b670b7fbc447c7d32f6a10c2484b63caa595d
Tysuiku <Timothydong41@gmail.com>
2022-11-20 00:02:14 -0500

    hangman_halfwaydone

--- practice_2/hangman_project 2/lib/hangman.rb
+++ practice_2/hangman_project 2/lib/hangman.rb
@@ -1,4 +1,38 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+  
+  def self.random_word
+    DICTIONARY.sample
+  end
 
+  attr_reader :guess_word, :attempted_chars, :remaining_incorrect_guesses
+  def initialize
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length){ "_" }
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  def already_attempted?(char)
+    @attempted_chars.include?(char)
+  end
+
+  def get_matching_indices(char)
+    arr = []
+    @secret_word.each_char.with_index do |ele, i|
+      if ele == char 
+        arr << i
+      end
+    end
+
+    arr
+  end
+
+  def fill_indices(char, arr)
+    arr.each do |idx|
+    @guess_word[idx] = char
+    end
+  end
+
+  
 end
